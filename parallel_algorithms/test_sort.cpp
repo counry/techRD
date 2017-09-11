aa99d9facf0f9bb174df9eb06085d7b0bc23cbe9
counry <counry@github.com>
2017-09-11 20:50:40 +0800

    fix bug

--- parallel_algorithms/test_sort.cpp
+++ parallel_algorithms/test_sort.cpp
@@ -4,13 +4,14 @@
 #include <time_util.hpp>
 #include <ctime>
 #include <parallel/algorithm>
+#include <vector>
 
 int main()
 {
     std::srand(std::time(0)); // use current time as seed for random generator
 
     std::vector<int> random_vector;
-    unsigned int vec_size = 500000;
+    unsigned int vec_size = 50000000;
     for (unsigned int i = 0; i < vec_size; i++) {
         int random_variable = std::rand();
         random_vector.push_back(random_variable);
@@ -25,19 +26,18 @@ int main()
 
     std::vector<int> random_vector_stable_sort(random_vector.begin(), random_vector.end());
     timer.reset();
-    std::stable_sort((random_vector_stable_sort.begin(), random_vector_stable_sort.end());
+    std::stable_sort(random_vector_stable_sort.begin(), random_vector_stable_sort.end());
     timer.write_text_to_screen("stable_sort random vector");
-
+    
     std::vector<int> random_vector_parallel_sort(random_vector.begin(), random_vector.end());
     timer.reset();
-    __gnu_parallel::sort((random_vector_parallel_sort.begin(), random_vector_parallel_sort.end());
+    __gnu_parallel::sort(random_vector_parallel_sort.begin(), random_vector_parallel_sort.end());
     timer.write_text_to_screen("parallel_sort random vector");
 
     std::vector<int> random_vector_parallel_stable_sort(random_vector.begin(), random_vector.end());
     timer.reset();
-    __gnu_parallel::stable_sort((random_vector_parallel_stable_sort.begin(), random_vector_parallel_stable_sort.end());
+    __gnu_parallel::stable_sort(random_vector_parallel_stable_sort.begin(), random_vector_parallel_stable_sort.end());
     timer.write_text_to_screen("parallel_stable_sort random vector");
-
     return 0;
 
 }
