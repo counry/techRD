1c5e25d28ddbcf1422bcc00e2e2df4cc3a7102aa
zhounianhua <zhounianhua@meituan.com>
2017-09-18 14:20:07 +0800

    add test case

--- osm/lat_lon.cpp
+++ osm/lat_lon.cpp
@@ -5,23 +5,24 @@
 #include <algorithm>
 #include <parallel/algorithm>
 #include <vector>
+#include <array>
 
-uint32_t ilat(double lat)
+uint32_t ilatitude(double lat)
 {
     return (lat + 91.0)*10000000+0.5;
 }
 
-int32_t ilon(double lon)
+int32_t ilongitude(double lon)
 {
     return lon*10000000 + (lon > 0 ? 0.5 : -0.5);
 }
 
-double lon(int32_t ilon)
+double longitude(int32_t ilon)
 {
     return ((double)ilon)/10000000;
 }
 
-double lat(uint32_t ilat)
+double latitude(uint32_t ilat)
 {
     return ((double)ilat)/10000000 - 91.0;
 }
@@ -29,10 +30,42 @@ double lat(uint32_t ilat)
 
 int main()
 {
-    double lat_ = 48.1245160;
-    double lon_ = -1.6093711;
-    std::cout << "transform lat " << lat_ << " to " << lat(ilat(lat_)) << std::endl;
-    std::cout << "transform lon " << lon_ << " to " << lon(ilon(lon_)) << std::endl;
+    double lat = 0.0;
+    double lon = 0.0;
+    std::vector<std::array<double, 2>> coords;
+
+    lat = 23.1141643;
+    lon = 121.0068784;
+    coords.push_back({lat, lon});
+
+    lat = 48.1245160;
+    lon = -1.6093711;
+    coords.push_back({lat, lon});
+
+    lat = 48.1254138;
+    lon = -1.6097754;
+    coords.push_back({lat, lon});
+
+    lat = 55.0809291;
+    lon = -118.8107007;
+    coords.push_back({lat, lon});
+
+    lat = -72.883245;
+    lon = 35.733876;
+    coords.push_back({lat, lon});
+
+    lat = -55.0658177;
+    lon = -67.4323684;
+    coords.push_back({lat, lon});
+    
+
+    for (auto c : coords) {
+        double _lat = c.front();
+        double _lon = c.back();
+        std::cout << "transform lat " << _lat << " to " << latitude(ilatitude(_lat)) << std::endl;
+        std::cout << "transform lon " << _lon << " to " << longitude(ilongitude(_lon)) << std::endl;
+    }
+
     return 0;
 }
 
