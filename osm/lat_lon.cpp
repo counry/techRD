f02c3468af2119e3025f58eea834cc23c0f794e8
counry <counry@github.com>
2017-09-18 11:49:20 +0800

    fix bug

--- osm/lat_lon.cpp
+++ osm/lat_lon.cpp
@@ -1,28 +1,27 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <time_util.hpp>
 #include <ctime>
 #include <algorithm>
 #include <parallel/algorithm>
 #include <vector>
 
-uint32 ilat(double lat)
+uint32_t ilat(double lat)
 {
     return (lat + 91.0)*10000000+0.5;
 }
 
-int32 ilon(double lon)
+int32_t ilon(double lon)
 {
     return lon*10000000 + (lon > 0 ? 0.5 : -0.5);
 }
 
-double lon(int32 ilon)
+double lon(int32_t ilon)
 {
     return ((double)ilon)/10000000;
 }
 
-double lat(uint32 ilat)
+double lat(uint32_t ilat)
 {
     return ((double)ilat)/10000000 - 91.0;
 }
@@ -30,11 +29,10 @@ double lat(uint32 ilat)
 
 int main()
 {
-    double lat = 48.1245160;
-    double lon = -1.6093711;
-    std::cout << "transform lat " << lat << " to " << lat(ilat(lat)) << std::endl;
-    std::cout << "transform lon " << lon << " to " << lon(ilon(lon)) << std::endl;
+    double lat_ = 48.1245160;
+    double lon_ = -1.6093711;
+    std::cout << "transform lat " << lat_ << " to " << lat(ilat(lat_)) << std::endl;
+    std::cout << "transform lon " << lon_ << " to " << lon(ilon(lon_)) << std::endl;
     return 0;
-
 }
 
